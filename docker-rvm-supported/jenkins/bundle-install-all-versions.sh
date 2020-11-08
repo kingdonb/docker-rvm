1872c598b2865a7744d93ee539999c0013c93a2c
Kingdon Barrett <kingdon.b@nd.edu>
2020-11-08 14:02:20 -0500

    squash with cache

--- docker-rvm-supported/jenkins/bundle-install-all-versions.sh
+++ docker-rvm-supported/jenkins/bundle-install-all-versions.sh
@@ -8,4 +8,11 @@ for ver in $RUBY_VERSIONS; do
   rvm $ver@global do gem update --system
   rvm $ver@global do bundle install
   rvm $ver@global do bundle clean --force
+
+done
+
+for ver in $RUBY_VERSIONS; do
+  SHORT_VER=${ver%.*}
+  mkdir -p /tmp/cache/$SHORT_VER
+  cp -r /usr/local/rvm/rubies/ruby-$ver/lib/ruby/gems/$SHORT_VER.0/* /tmp/cache/$SHORT_VER/
 done

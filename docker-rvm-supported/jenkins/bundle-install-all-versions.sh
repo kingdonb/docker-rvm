e0a5738e328428a402634d3879ad02ddec81a60c
Kingdon Barrett <kingdon.b@nd.edu>
2020-11-08 13:33:10 -0500

    gem update bundler
    since gem install bundler generates an error message given the latest
    bundler is already installed (but gem update bundler doesn't)

--- docker-rvm-supported/jenkins/bundle-install-all-versions.sh
+++ docker-rvm-supported/jenkins/bundle-install-all-versions.sh
@@ -4,7 +4,7 @@ source /etc/profile.d/rvm.sh
 echo 'gem: --no-document' > ~/.gemrc
 
 for ver in $RUBY_VERSIONS; do
-  rvm $ver@global do gem install bundler
+  rvm $ver@global do gem update bundler
   rvm $ver@global do gem update --system
   rvm $ver@global do bundle install
   rvm $ver@global do bundle clean --force

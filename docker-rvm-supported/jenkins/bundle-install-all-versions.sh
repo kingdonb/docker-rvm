9051b283ff657f03a6a6808f2b546fab28a3139f
Kingdon Barrett <kingdon.b@nd.edu>
2020-11-08 12:35:03 -0500

    bundle update --bundler

--- docker-rvm-supported/jenkins/bundle-install-all-versions.sh
+++ docker-rvm-supported/jenkins/bundle-install-all-versions.sh
@@ -3,6 +3,7 @@
 source /etc/profile.d/rvm.sh
 
 for ver in $RUBY_VERSIONS; do
+  rvm $ver@global do bundle update --bundler
   rvm $ver@global do bundle install
   rvm $ver@global do bundle clean --force
 done

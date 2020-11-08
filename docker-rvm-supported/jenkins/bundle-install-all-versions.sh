a95847c890e1a1d1884d4a13b542dd42b8507424
Kingdon Barrett <kingdon.b@nd.edu>
2020-11-08 13:31:06 -0500

    gem install bundler

--- docker-rvm-supported/jenkins/bundle-install-all-versions.sh
+++ docker-rvm-supported/jenkins/bundle-install-all-versions.sh
@@ -5,7 +5,7 @@ echo 'gem: --no-document' > ~/.gemrc
 
 for ver in $RUBY_VERSIONS; do
   rvm $ver@global do gem update --system
-  # rvm $ver@global do bundle update --bundler
+  rvm $ver@global do gem install bundler
   rvm $ver@global do bundle install
   rvm $ver@global do bundle clean --force
 done

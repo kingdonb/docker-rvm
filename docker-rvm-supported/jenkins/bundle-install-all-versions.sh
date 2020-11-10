21543315502374feb7ab23d9bb399f7bd7898755
Kingdon Barrett <kingdon.b@nd.edu>
2020-11-10 09:51:22 -0500

    clean up minor inconsistency, redundant "rvm use"
    I think we only need to invoke the global gemset once per ruby version

--- docker-rvm-supported/jenkins/bundle-install-all-versions.sh
+++ docker-rvm-supported/jenkins/bundle-install-all-versions.sh
@@ -4,11 +4,12 @@ source /etc/profile.d/rvm.sh
 echo 'gem: --no-document' > ~/.gemrc
 
 for ver in $RUBY_VERSIONS; do
-  rvm $ver@global do gem update bundler
-  rvm $ver@global do gem update --system
-  rvm $ver@global do bundle install
-  rvm $ver@global do bundle clean --force
+  rvm $ver@global
 
+  gem update bundler
+  gem update --system
+  bundle install
+  bundle clean --force
 done
 
 for ver in $RUBY_VERSIONS; do

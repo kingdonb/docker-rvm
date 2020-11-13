43bb903462d17ee370a8470ab4b9a3d3315b4358
Kingdon Barrett <kingdon.b@nd.edu>
2020-11-13 07:27:42 -0500

    looks like this level of explicit is required
    it worked without it for a little while, because cache masked the issue,
    but a fresh start with a clean cache revealed the problem

--- docker-rvm-supported/jenkins/bundle-install-all-versions.sh
+++ docker-rvm-supported/jenkins/bundle-install-all-versions.sh
@@ -6,10 +6,12 @@ echo 'gem: --no-document' > ~/.gemrc
 for ver in $RUBY_VERSIONS; do
   rvm $ver@global
 
-  gem update bundler
-  gem update --system
-  bundle install
-  bundle clean --force
+  rvm $ver@global do gem update --system
+  rvm $ver@global do gem update bundler
+  rvm $ver@global do gem list
+
+  rvm $ver@global do bundle install
+  rvm $ver@global do bundle clean --force
 done
 
 set -euo pipefail
@@ -18,7 +20,5 @@ set -x
 for ver in $RUBY_VERSIONS; do
   SHORT_VER=${ver%.*}
   mkdir -p /tmp/cache/$SHORT_VER
-  ls -l /tmp/cache/
-  ls -l /tmp/cache/$SHORT_VER
   cp -r /usr/local/rvm/rubies/ruby-$ver/lib/ruby/gems/$SHORT_VER.0/* /tmp/cache/$SHORT_VER/
 done

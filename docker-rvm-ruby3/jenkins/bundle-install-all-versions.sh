9b2fcb3279184379d426a65f1cbf54129fe1fd07
Kingdon Barrett <kingdon@weave.works>
2023-12-26 21:42:49 +0000

    fixup #6
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- docker-rvm-ruby3/jenkins/bundle-install-all-versions.sh
+++ docker-rvm-ruby3/jenkins/bundle-install-all-versions.sh
@@ -20,5 +20,5 @@ set -x
 for ver in $RUBY_VERSIONS; do
   SHORT_VER=${ver%.*}
   mkdir -p /tmp/cache/$SHORT_VER
-  # cp -r /usr/local/rvm/rubies/ruby-$ver/lib/ruby/gems/$SHORT_VER.0/* /tmp/cache/$SHORT_VER/
+  cp -r /usr/local/rvm/rubies/ruby-$ver/lib/ruby/gems/$SHORT_VER.0/* /tmp/cache/$SHORT_VER/
 done
